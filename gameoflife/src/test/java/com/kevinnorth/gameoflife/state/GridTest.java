26d753341faaa4255a83db910f56daf2e483d314
Kevin North <kevinjamesnorth@outlook.com>
2024-01-09 17:47:09 -0600

    test(Grid): finish testing Grid

--- gameoflife/src/test/java/com/kevinnorth/gameoflife/state/GridTest.java
+++ gameoflife/src/test/java/com/kevinnorth/gameoflife/state/GridTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+
 public class GridTest {
   @Test
   public void IfConstructedWithDimensionsThenCreatesGridOfDeadCells() {
@@ -14,5 +16,45 @@ public class GridTest {
         assertEquals(false, grid.getCell(x, y).alive());
       }
     }
+  }
+
+
+  @Test
+  public void IfConstructedWithCollectionOfCellsThenCreatesGridOfMatchingCells() {
+    boolean[][] initialState = {
+      {false, true, false},
+      {false, false, true},
+      {true, true, true}
+    };
+    var originalCells = new ArrayList<ArrayList<Cell>>(initialState.length);
+
+    for (int x = 0; x < initialState.length; x++) {
+      var column = new ArrayList<Cell>(initialState[x].length);
+
+      for(int y = 0; y < initialState[x].length; y++) {
+        column.add(new Cell(initialState[x][y]));
+      }
+
+      originalCells.add(column);
+    }
+
+    var grid = new Grid(originalCells);
+
+    for (int x = 0; x < initialState.length; x++) {
+      for(int y = 0; y < initialState[x].length; y++) {
+        assertEquals(initialState[x][y], grid.getCell(x, y).alive());
+      }
+    }
+  }
+
+  @Test
+  public void GetHieghtAndGetWidth() {
+    int height = 21;
+    int width = 43;
+
+    var grid = new Grid(width, height);
+
+    assertEquals(height, grid.getHeight());
+    assertEquals(width, grid.getWidth());
   }
 }
